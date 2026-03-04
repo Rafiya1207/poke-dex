b8ae12a17dcd4fd3e342bcc8526fe5a24c0c45f0
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2026-03-04 18:07:52 +0530

    refactors serve pokemon to apply filters

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,5 +1,3 @@
-import { capitalize } from "./utils.js";
-
 const links = [
   "all",
   "bug",
@@ -22,33 +20,25 @@ const links = [
   "water",
 ];
 
-const filterPokemonsByType = (pokemons, type) =>
-  pokemons.filter(({ types }) => types.includes(type));
-
-const filterPokemonsByName = (pokemons, pokemon) =>
-  pokemons.filter(({ name }) => name.startsWith(pokemon));
+const applyFilters = (pokemons, { type, name }) =>
+  pokemons.filter(pokemon =>
+    pokemon.types.includes(type) && pokemon.name.startsWith(name)
+  )
 
 export const servePokemons = async (c) => {
-
-  const { name } = await c.req.param();
-  const searchPokemon = c.req.query("pokemon");
-
-  const activePokemon = name === undefined ? "all" : name;
+  const { type } = await c.req.param();
+  const name = c.req.query("pokemon") || '';
+  const activePokemon = type === undefined ? "all" : type;
 
   if (!links.includes(activePokemon)) {
     return c.json({ error: "not found" }, 404);
   }
 
   const pokemons = c.get("pokemons");
-  const renderPokemons = c.get("renderPokemons");
 
   const data = activePokemon === "all"
     ? pokemons
-    : filterPokemonsByType(pokemons, activePokemon);
+    : applyFilters(pokemons, { type, name });
 
-  const foundPokemons = searchPokemon !== undefined
-    ? filterPokemonsByName(data, searchPokemon)
-    : data;
-
-  return c.json(foundPokemons, 200);
+  return c.json(data, 200);
 };
